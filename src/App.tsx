e9b317496b43d2792cc1abe8f13f08b45fc4a820
Erick <emc.7@hotmail.com>
2022-04-04 09:14:02 -0300

    primeiro commit.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <Button variant="contained">Hello World</Button>
+  );
+}
+
+export default App;
